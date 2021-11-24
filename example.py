f205d3b2ce179092097ba29ce3dae2e991e21f04
sergiocamacho09 <trabajointernetalameda@gmail.com>
2021-11-24 19:07:40 +0100

    primeros atributos

--- example.py
+++ example.py
@@ -21,11 +21,28 @@ for i in range(0, len(url_list)):
     response = requests.get(url_list[i])
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    name_box = soup.title.string
+    # name_box = soup.title.string
+    # Obtención del título en castellano
+    name_box = soup.find('h1', attrs={'data-testid': 'hero-title-block__title'}).get_text()
+    # Obtención del título original (en su idioma original)
+    original_title = soup.find('div', attrs={'data-testid': 'hero-title-block__original-title'})
 
-    print(name_box)
-    print('-----------------------------------')
+    genres = soup.find('div', attrs={'data-testid': 'genres'}).find_all('span', attrs={'class': 'ipc-chip__text'})
+    genres_list = []
+    
+    for i in range(len(genres)):
+        genres_list.append(genres[i].get_text())
+    print("Genres ", genres_list)
+    if(original_title is not None):
+        original_title = original_title.get_text().split(': ')
+        print(str(name_box) + " original title: " + str(original_title[1]))
+    else:
+        print(str(name_box) )
 
+    # print(name_box)
+    
+    #print('-----------------------------------')
+    
 # name_box = soup.find('h1', attrs={'class': 'TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG'}).get_text()
 # print(name_box)
 # '''print(soup.find_all(class_h1))'''
